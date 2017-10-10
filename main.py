f56866644e3433e7d781b1e067f67b40c426e3f7
Ryota Yamada <roy29fuku@gmail.com>
2017-10-10 18:22:40 +0900

    change message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hello, Azure!'
+    return 'Hello, Azure Web App!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
